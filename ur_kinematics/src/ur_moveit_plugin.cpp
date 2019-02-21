a3707d88c573dbdf8aea5f792b81e135aebb62de
Leonhard Euler <leonhard.euler@stiima.cnr.it>
2019-02-21 12:26:41 +0100

    euler

--- ur_kinematics/src/ur_moveit_plugin.cpp
+++ ur_kinematics/src/ur_moveit_plugin.cpp
@@ -324,7 +324,7 @@ bool URKinematicsPlugin::initialize(const std::string &robot_description,
   epsilon_ = epsilon;
 
   private_handle.param<std::string>("arm_prefix", arm_prefix_, "");
-
+  arm_prefix_="ur10_";
   ur_joint_names_.push_back(arm_prefix_ + "shoulder_pan_joint");
   ur_joint_names_.push_back(arm_prefix_ + "shoulder_lift_joint");
   ur_joint_names_.push_back(arm_prefix_ + "elbow_joint");
